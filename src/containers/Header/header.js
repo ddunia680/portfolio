cdd7fbe80fb1f08b2e1739b1fc11d1f8f238b786
Dunia Dunia <ddunia680@gmail.com>
2022-11-29 01:58:58 +0300

    ad dropdown menu for small screens

--- src/containers/Header/header.js
+++ src/containers/Header/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavItems from '../../components/navigation/navItems';
 import GoButton from '../../components/GoButton/goButton';
 
@@ -6,7 +6,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl, faX } from '@fortawesome/free-solid-svg-icons'
 import logo from '../../images/logo.png'
 
-function header(props) {
+function Header(props) {
+    let [showDropUI, setShowDropUI] = useState(false);
+
+    const showDrop = () => {
+        setShowDropUI(true);
+    }
+
+    const hideDrop = () => {
+        setShowDropUI(false);
+    }
+
     return (
         <header className='fixed top-0 w-screen h-20 bg-darkBlue text-white items-center mx-auto flex flex-row justify-between px-3 shadow-md shadow-violet lg:px-40'>
             <div className='w-[50px] h-[70%]'>
@@ -19,12 +29,23 @@ function header(props) {
                 </GoButton>
             </div>
             <div className='h-80% block md:hidden'>
-                <FontAwesomeIcon icon={faListUl} className='text-fromLogo text-3xl'/>
-                {/* <FontAwesomeIcon icon={faX} className='text-fromLogo text-3xl'/> */}
+                {!showDropUI ? 
+                <FontAwesomeIcon icon={faListUl} className='text-fromLogo text-3xl'onClick={showDrop}/>
+                :
+                <FontAwesomeIcon icon={faX} className='text-fromLogo text-3xl' onClick={hideDrop}/> }
             </div>
+            {showDropUI ? 
+            <div className='absolute bg-darkBlue right-0 top-20 px-10 border-l-2 border-r-2 border-b-2  border-violet'>
+                <ul className='flex flex-col space-y-2'>
+                    <li className='hover:text-fromLogo py-1'><a href='#'>About</a></li>
+                    <li className='hover:text-fromLogo py-1'><a href='#'>Projects</a></li>
+                    <li className='hover:text-fromLogo py-1'><a href='#'>Contacts</a></li>
+                </ul>
+            </div>
+            : null}
             
         </header>
     );
 }
 
-export default header;
+export default Header;
