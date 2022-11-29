55249286f1cd23eaf387f3b3372f9c381cacc7d6
Dunia Dunia <ddunia680@gmail.com>
2022-11-29 18:39:28 +0300

    add whatsapp Api

--- src/containers/Presentation/presentation.js
+++ src/containers/Presentation/presentation.js
@@ -16,7 +16,11 @@ function Presentation(props) {
                     <p className='text-fromLogo font-bold '>Hello there, I'm </p>
                     <h1 className='text-[2.000rem] text-white font-extrabold font-sans md:text-[5.052rem]'>Dunia Dunia</h1>
                     <p>I'm a big believe in technology as a key to easing every aspect of the human activities through scalable software, well designed and utility based</p>
-                    <button className='px-3 w-50 mx-auto border-2 border-fromLogo p-2 rounded-lg transition-all duration-500 text-fromLogo hover:bg-lightButton hover:text-fromLogo hover:transition-all hover:duration-500'>Let's Connect</button>
+                    <button className='px-3 w-50 mx-auto border-2 border-fromLogo p-2 rounded-lg transition-all duration-500 text-fromLogo hover:bg-lightButton hover:text-fromLogo hover:transition-all hover:duration-500'>
+                        <a href='https://api.whatsapp.com/send?phone=256788306651&text=Thanks%20for%20contacting'>
+                            Let's Connect
+                        </a>
+                    </button>
                 </div>
         </motion.div>
     );
